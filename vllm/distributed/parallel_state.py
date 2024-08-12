214a668023b2c78b65bbf580f3c835f5ace018cc
Johannes M Dieterich <johannes.dieterich@amd.com>
2024-08-12 16:48:01 -0500

    Document crossover

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -199,6 +199,7 @@ def initialize_model_parallel(
     if _ENABLE_CUSTOM_ALL_REDUCE:
         from vllm.distributed.device_communicators.custom_all_reduce import (
             CustomAllreduce)
+        # max size defaults to 8 MiB, increase to 16 MiB on ROCm due to later crossover
         if is_hip():
             _TP_CA_COMMUNICATOR = CustomAllreduce(
                 group=_TP_CPU_GROUP,
